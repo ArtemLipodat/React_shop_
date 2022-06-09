c0f5662401d863e55aa5f7bd80923a442a8ef9ca
Artem <a.lipodat@mail.ru>
2022-06-09 16:47:25 +0400

    adaptive v2

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
             <div className="footer-copy">{date.getFullYear()} &copy; Вкус и Цвет</div>
             <div className="footer-icons">
                 <a target='_blank' href="https://api.whatsapp.com/send?phone=79501573115&text=%D0%94%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B4%D0%B5%D0%BD%D1%8C!"><img src="./img/whatsapp.png" alt=""/></a>
-                <a href="/"><img src="./img/instagram.png" alt=""/></a>
+                <a target="_blank" href="https://instagram.com/vkus_i_zvet?igshid=YmMyMTA2M2Y="><img src="./img/instagram.png" alt=""/></a>
                 <a target="_blank" href="https://vk.com/club209663538"><img src="./img/vk.png" alt=""/></a>
             </div>
             <div className="footer-company">
